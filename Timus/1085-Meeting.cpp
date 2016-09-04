b030304a156fa01b9144873e6b78badfdf6cfaa0
Surendran Kanagaraj <surend35@gmail.com>
2016-09-04 12:39:00 +0530

    Update 1085-Meeting.cpp

--- Timus/1085-Meeting.cpp
+++ Timus/1085-Meeting.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <map>
 #include <climits>
+#include <cstring>
 #include <unordered_map>
 #include <utility>
 #include <vector>
@@ -12,105 +13,93 @@
 #define pii pair<int,int>
 #define ll long long
 #define x first
+#define inf INT_MAX
 #define y second
 #define rep(i,a,b) for(int i=a; i<=b; i++)
 #define rrep(i,a,b) for(int i=a; i>=b; i--)
 using namespace std;
-vector<int> v[100];
-int subnet[100][100][4];
-int sz[100];
-int k,n;
-vector<int> decode(int i,string s1)
-{
-        int no=0;
-        vector<int> a;
-        rep(j,0,(int)s1.size()-1)
-        {
-                if(s1[j]=='.')
-                {
-                        a.pb(no);
-                        no=0;
-                }
-                else
-                        no=no*10+(s1[j]-'0');
-        }
-        a.pb(no);
-        return a;
-}
+vector<pii> v[150];
 int main()
 {
         ios::sync_with_stdio(false);
-        std::cin >> n;
-        rep(i,0,n-1)
+        int n,m;
+        std::cin >> n>>m;
+        rep(i,0,m-1)
         {
-                std::cin >> k;
-                sz[i]=k;
-                rep(l,0,k-1)
+                int l;
+                std::cin >> l;
+                int prev;
+                std::cin >> prev;
+                rep(j,0,l-2)
                 {
-                        string s1,s2;
-                        std::cin >> s1>>s2;
-                        vector<int> c=decode(i,s1);
-                        vector<int> d=decode(i,s2);
-                        rep(j,0,3)
-                        {
-                                subnet[i][l][j]=c[j]&d[j];
-                        }
+                        int x;
+                        std::cin>> x;
+                        v[prev].pb({x,i});
+                        v[x].pb({prev,i});
+                        prev=x;
                 }
         }
-        int src,dest;
-        std::cin >> src>>dest;
-        rep(i,0,n-1)
+        int k;
+        std::cin >> k;
+        int tot[150];
+        int start;
+        int pay[150];
+        int visit[150][150];
+        memset(visit,-1,sizeof(visit));
+        queue<vector<int> > q;
+        rep(i,0,k-1)
         {
-                rep(j,0,n-1)
-                {
-                        rep(g,0,sz[i]-1)
-                        {
-                                rep(h,0,sz[j]-1)
-                                {
-                                        int fl=1;
-                                        rep(r,0,3)
-                                        {
-                                                if(subnet[i][g][r]!=subnet[j][h][r])
-                                                        fl=0;
-                                        }
-                                        if(fl)
-                                                v[i].pb(j);
-                                }
-                        }
-                }
-        }
-        queue<pair<int,vector<int> > > q;
-        int visit[100]={0};
-        rep(i,0,sz[src-1]-1)
-        {
-                q.push({src-1,{src-1}});
+                std::cin >> tot[i];
+                std::cin >> start;
+                std::cin >> pay[i];
+                q.push({start,i,0,-1});
+                visit[i][start]=0;
         }
         while(!q.empty())
         {
-                vector<int> path=q.front().y;
-                int last=q.front().x;
-                visit[last]=1;
-                if(last==dest-1)
-                {
-                        std::cout << "Yes" << "\n";
-                        rep(i,0,path.size()-1)
-                        {
-                                std::cout << path[i]+1<<" ";
-                        }
-                        return 0;
-                }
+                int node=q.front()[0];
+                int ppl=q.front()[1];
+                int cost=q.front()[2];
+                int route=q.front()[3];
                 q.pop();
-                rep(i,0,v[last].size()-1)
+                for(auto it :v[node])
                 {
-                        int node=v[last][i];
-                        if(!visit[node])
+                        int amt=(it.y!=route)  ? 4 : 0;
+                        if(pay[ppl]==1)
+                                amt=0;
+                        if(visit[ppl][it.x]==-1)
                         {
-                                vector<int> temp=path;
-                                temp.pb(node);
-                                q.push({node,temp});
+                                visit[ppl][it.x]=cost+amt;
+                                q.push({it.x,ppl,cost+amt,it.y});
                         }
                 }
         }
-        std::cout << "No" << "\n";
+        int stop=0;
+        int ans=INT_MAX;
+        rep(i,1,n)
+        {
+                int cost=0;
+                rep(j,0,k-1)
+                {
+                        if(visit[j][i]>tot[j] or visit[j][i]==-1)
+                        {
+                                cost=inf;
+                                break;
+                        }
+                        else
+                        {
+                                cost+=visit[j][i];
+                        }
+                }
+                if(cost<ans)
+                {
+                        stop=i;
+                        ans=cost;
+                }
+        }
+        if(ans==inf)
+                std::cout << "0" << std::endl;
+        else
+                std::cout << stop<<" "<<ans << std::endl;
         return 0;
 }
